bac8032b0b3231ba612e43dfb4d2a98ee5a223e0
Sminotti <sebastianminotti@gmail.com>
2026-03-09 15:24:59 -0300

    se implementa firebase

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost/Proyectos_DeMentes/ComunidadIFTS/BackEnd/api'
+  apiUrl: 'https://comunidadifts.infinityfreeapp.com/api',
+  firebase: {
+    apiKey: "AIzaSyCP0DGVHLfLfWRI6pbUpzAkCeQ0VVWR9K0",
+    authDomain: "comunidadifts.firebaseapp.com",
+    projectId: "comunidadifts",
+    storageBucket: "comunidadifts.firebasestorage.app",
+    messagingSenderId: "266707300773",
+    appId: "1:266707300773:web:c8dc908a0d8f0a0267f84c"
+  }
 };
